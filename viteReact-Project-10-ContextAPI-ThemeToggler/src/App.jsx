139f46df4daeafe28d6947e7ede0d8779ee03f77
Zaman <zamanofficial1980@gmail.com>
2024-04-27 14:22:35 +0530

    all react projects

---
+++ viteReact-Project-10-ContextAPI-ThemeToggler/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className='bg-slate-700'>Zaman</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
