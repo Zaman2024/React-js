303fddeb563687ccfc69c57b113bf8feeff6b3c1
Zaman <zamanofficial1980@gmail.com>
2024-05-02 23:36:20 +0530

    modified some cods

--- viteReact-P11-Todo_contextAPI/src/App.jsx
+++ viteReact-P11-Todo_contextAPI/src/App.jsx
@@ -38,19 +38,23 @@ function App() {
 
   return (
     <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
-      <div>
-        <div>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+        <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+        <div className="mb-4">
           <TodoForm />
         </div>
 
-        <div>
+        <div className="flex flex-wrap gap-y-3">
           {/*Loop and add todo items here*/}
           {todos.map((todo) => (
             <div key ={todo.id}
+            className='w-full'
             >
               <TodoItems todo={todo} />
             </div>
           ))}
+        </div>
         </div>
       </div>
     </TodoProvider>
