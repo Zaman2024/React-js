c73a4ad26277bc5802106329852409d44fcd24e8
Zaman <zamanofficial1980@gmail.com>
2024-05-06 21:50:06 +0530

    Adds some new code

--- vite-todo-project/src/App.jsx
+++ vite-todo-project/src/App.jsx
@@ -11,8 +11,16 @@ const addTodo = (todo) => {
   setTodos((prev) => [{id: Date.now(), ...todo}, ...prev])
 }
 
+const updateTodo = (id, todo) => {
+  setTodos((prev) => prev.map((prevTodo)=> prevTodo.id === id ? todo : prevTodo))
+}
+
+const deleteTodo = (id) => {
+  setTodos((prevTodo) =>{})
+}
+
   return (
-    <TodoProvider value = {{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
+    <TodoProvider value = {{todos, addTodo, updateTodo, deleteTodo}}>
 
       <div>
         <TodoForm/>
--- vite-todo-project/src/Components/TodoForm.jsx
+++ vite-todo-project/src/Components/TodoForm.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function TodoForm() {
+
+  const [todo, setTodo] = useState('')
+
+
+  const add = () => {
+
+  }
   return (
-    <div>
+    <form onSubmit={add}
+    className='bg-slate-300 '
+    >
       <h1 className='bg-slate-800 text-white'>Zaman</h1>
-    </div>
+      <input 
+        type='text'
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        placeholder='Write Todo'
+        className='my-5 p-1 outline-none rounded-s-lg'
+      />
+      <button type='Submit' className='bg-green-500 text-white px-3 py-1 rounded-e-lg' >Add</button>
+    </form>
   )
 }
 
