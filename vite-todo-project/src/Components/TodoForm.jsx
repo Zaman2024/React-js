d2131de700dc598a034b687aefac052684380b0d
Zaman <zamanofficial1980@gmail.com>
2024-05-07 23:08:10 +0530

    modified some codes

--- vite-todo-project/src/Components/TodoForm.jsx
+++ vite-todo-project/src/Components/TodoForm.jsx
@@ -1,13 +1,22 @@
 import React,{useState} from 'react'
+import { useTodo } from '../ContextAPI/TodoContext'
 
 function TodoForm() {
 
-  const [todo, setTodo] = useState('')
+  const [todo, setTodo] = useState([])
+  const {addTodo} = useTodo()
 
 
-  const add = () => {
+  const add = (e) => {
+    e.preventDefault()
 
+    if(!todo) return
+
+    addTodo({todo, completed : false})
+
+    setTodo("")
   }
+  
   return (
     <form onSubmit={add}
     className='bg-slate-300 '
