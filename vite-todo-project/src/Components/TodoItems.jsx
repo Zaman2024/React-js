7919da5c5b7cd93789f25f7d68960c717c673af7
Zaman <zamanofficial1980@gmail.com>
2024-05-08 23:51:36 +0530

    modified some codes

--- vite-todo-project/src/Components/TodoItems.jsx
+++ vite-todo-project/src/Components/TodoItems.jsx
@@ -6,9 +6,7 @@ function TodoItems({todo}) {
   const [TodoMsg, setTodoMsg] = useState(todo.todo)
   const {updateTodo, deleteTodo, toggleComplete} = useTodo()
   return (
-    <div>
-      <h1 className='bg-yellow-700 text-white'>Todo List</h1>
-
+    <div className='m-1 px-2 py-1'>
       <input
         type='checkbox'
         checked ={todo.completed}
