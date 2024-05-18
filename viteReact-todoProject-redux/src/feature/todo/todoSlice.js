20a6d19453e719563b35a3d1e33a22c88ac6bc94
Zaman <zamanofficial1980@gmail.com>
2024-05-18 23:58:55 +0530

    adds some new codes

--- viteReact-todoProject-redux/src/feature/todo/todoSlice.js
+++ viteReact-todoProject-redux/src/feature/todo/todoSlice.js
@@ -1,1 +1,32 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+    todos:[
+        {
+            id : 1,
+            text : 'Hello World'
+        }
+    ]
+}
+
+export const todoSlice = createSlice({
+    name: 'Todo',
+    initialState,
+    reducers : {
+        addTodo :( state, action) => {
+            const todo = {
+                id : nanoid(),
+                text : action.payload
+            }
+            state.todos.push(todo)
+        },
+
+        removeTodo : (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        },
+
+        updateTodo : (state, action) => {
+            state.todos = state.todos.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
+        }
+    }
+})
