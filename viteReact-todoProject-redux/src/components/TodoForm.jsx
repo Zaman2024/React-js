752f31d6fe78963cbe2ab68a257f5731ed58cc15
Zaman <zamanofficial1980@gmail.com>
2024-05-21 15:18:50 +0530

    adds some new codes

--- viteReact-todoProject-redux/src/components/TodoForm.jsx
+++ viteReact-todoProject-redux/src/components/TodoForm.jsx
@@ -8,7 +8,7 @@ function TodoForm() {
 
 const handlerSubmit = (e) => {
     e.preventDefault()
-    
+
     dispatch(addTodo(input))
 }
 
@@ -20,8 +20,11 @@ const handlerSubmit = (e) => {
             value={input}
             placeholder='Write Todos'
             onChange={(e) =>setInput(e.target.value)}
+            className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
         />
-      <button type='submit'>Add</button>
+      <button type='submit'
+        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+      >Add</button>
     </form>
   )
 }
