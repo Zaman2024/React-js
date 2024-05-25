50a93db8f86f924aeccd6fb1930be0e72dca2636
Zaman <zamanofficial1980@gmail.com>
2024-05-25 21:44:01 +0530

    modified some code

--- viteReact-todoProject-redux/src/components/TodoList.jsx
+++ viteReact-todoProject-redux/src/components/TodoList.jsx
@@ -19,8 +19,6 @@ function TodoList({todo}) {
            <button
               className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50"
               onClick={() => {
-                  if (todo.completed) return;
-
                   if (isTodoEditable) {
                       updateTodo();
                   } else setIsTodoEditable((prev) => !prev);
