647bc38b9e016e5db5a805fe962a5d3583589480
Zaman <zamanofficial1980@gmail.com>
2024-05-26 14:56:06 +0530

    modified some codes

--- viteReact-todoProject-redux/src/components/TodoList.jsx
+++ viteReact-todoProject-redux/src/components/TodoList.jsx
@@ -9,26 +9,20 @@ function TodoList({todo}) {
     const todos = useSelector(state => state.todos)
     const dispatch = useDispatch()
 
+    const editTodo = () => {
+      updateTodo(todo.id, {...todo, todo: todoMsg})
+      setIsTodoEditable(false)
+    }
+
   return (
     <>
        <div  className='flex flex-col gap-2 border bg-slate-600 border-black/50 rounded-lg mx-60 px-3 py-1.5 shadow-sm shadow-white/50 duration-300  text-black list-none text-white'>
+
        {todos.map((todo) => (
         <li key={todo.id}>
            { todo.text}
-
+           
            <div className='flex float-right gap-2 '>
-           <button
-              className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50"
-              onClick={() => {
-                  if (isTodoEditable) {
-                      updateTodo();
-                  } else setIsTodoEditable((prev) => !prev);
-              }}
-              disabled={todo.completed}
-          >
-              {isTodoEditable ? "📁" : "✏️"}
-          </button>
-
            <button onClick={() => dispatch(removeTodo(todo.id))}
             className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
            >X</button>
