1d0d89ad7e4633399475770568eca974e5b134de
Zaman <zamanofficial1980@gmail.com>
2024-05-29 00:23:16 +0530

    edit some codes

--- viteReact-todoProject-redux/src/components/TodoList.jsx
+++ viteReact-todoProject-redux/src/components/TodoList.jsx
@@ -17,7 +17,7 @@ function TodoList({ todo }) {
   return (
     <>
       <div className="flex flex-col gap-2 border bg-slate-600 border-black/50 rounded-lg mx-60 px-3 py-1.5 shadow-sm shadow-white/50 duration-300  text-black list-none text-white">
-        {/* 
+        
        {todos.map((todo) => (
         <li key={todo.id}>
            { todo.text}
@@ -33,33 +33,9 @@ function TodoList({ todo }) {
            </div>
 
         </li>
-       ))} */}
+       ))}
 
-        <input
-          type="text"
-          className={`border outline-none w-full bg-transparent rounded-lg ${
-            isTodoEditable ? "border-black/10 px-2" : "border-transparent"
-          } `}
-          value={todoMsg}
-          onChange={(e) => setTodoMsg(e.target.value)}
-          readOnly={!isTodoEditable}
-        />
-        {/* Edit, Save Button */}
-        <button
-          className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50"
-          onClick={() => {
-
-            if (isTodoEditable) {
-              editTodo();
-            } else setIsTodoEditable((prev) => !prev);
-          }}
-        >
-          {isTodoEditable ? "📁" : "✏️"}
-        </button>
-
-        <button onClick={() => dispatch(removeTodo(todo.id))}
-            className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
-           >X</button>
+        
       </div>
     </>
   );
