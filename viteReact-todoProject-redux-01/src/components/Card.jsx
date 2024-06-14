d6181b4a80dde5409abb6842a3c16440df02c9ec
Zaman <zamanofficial1980@gmail.com>
2024-06-15 02:35:37 +0530

    adds some new codes

--- viteReact-todoProject-redux-01/src/components/Card.jsx
+++ viteReact-todoProject-redux-01/src/components/Card.jsx
@@ -1,63 +1,80 @@
 import React from "react";
 import { removeTodo, todoUpdated } from "../feature/todo/todoSlice";
 import { clearTodos, toggleInputform } from "../feature/todo/todoSlice";
-import TodoForm from "../components/TodoForm"
-import UpdateForm from "../components/UpdateForm"
-
+import TodoForm from "../components/TodoForm";
+import UpdateForm from "../components/UpdateForm";
+import TodoList from "../components/TodoList";
 
 import { useSelector, useDispatch } from "react-redux";
 
 function Card() {
-    const todos = useSelector((state) => state.todos)
-    const toggleForm = useSelector((state) => state.toggleForm)
-    const dispatch = useDispatch();
-    console.log(todos)
-
-
+  const todos = useSelector((state) => state.todos);
+  const toggleForm = useSelector((state) => state.toggleForm);
+  const dispatch = useDispatch();
+  console.log(todos);
 
   return (
     <div className="bg-slate-700  mx-60 p-5 rounded-xl border-4 border-white/50">
-      
       <div>
         <h1 className="text-white">Zaman</h1>
-        {toggleForm ? <TodoForm /> : <UpdateForm/>}
-     
+        {toggleForm ? <TodoForm /> : <UpdateForm />}
       </div>
-
-      <div>
-        <ul 
-      className="flex flex-col gap-2 border bg-slate-600 border-black/50 rounded-lg px-1 py-0.5 shadow-sm shadow-white/50 duration-200  text-black list-none text-white">
+      <ul 
+      >
         
        {todos.map((todo) => (
-        <li className="mt-0.5 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded text-white" 
+        <li className="mt-3 flex justify-between  bg-purple-800 px-4 py-2 rounded-xl text-white text-lg" 
         key={todo.id}>
-           { todo.text}
-           
+          { todo.text}
 
-           <div className='flex float-right gap-2 '>
-            <button 
-              onClick={() => dispatch(toggleInputform({
-                id : todo.id,
-                text : todo.text
-              }))}
-              className={`px-4 py-2 rounded ${toggleInputform ? "bg-blue-700":"bg-green-700"}` }
-            >
-              Edit
-            </button>
-
-           <button onClick={() => dispatch(removeTodo(todo.id))}
-            className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
-           >X</button>
-           </div>
+           <TodoList/>
 
         </li>
        ))}
 
         
       </ul>
-      </div>
-      <button className="bg-purple-700 text-white text-2xl px-3 py-1 rounded-xl mt-16 "
-      onClick={() => dispatch(clearTodos())}
+
+      {/* <div>
+        <ul className="flex flex-col gap-2 border bg-slate-600 border-black/50 rounded-lg px-1 py-0.5 shadow-sm shadow-white/50 duration-200  text-black list-none text-white">
+          {todos.map((todo) => (
+            <li
+              className="mt-0.5 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded text-white"
+              key={todo.id}
+            >
+              {todo.text}
+
+              <div className="flex float-right gap-2 ">
+                <button
+                  onClick={() =>
+                    dispatch(
+                      toggleInputform({
+                        id: todo.id,
+                        text: todo.text,
+                      })
+                    )
+                  }
+                  className={`px-4 py-2 rounded ${
+                    toggleInputform ? "bg-blue-700" : "bg-green-700"
+                  }`}
+                >
+                  Edit
+                </button>
+
+                <button
+                  onClick={() => dispatch(removeTodo(todo.id))}
+                  className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
+                >
+                  X
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div> */}
+      <button
+        className="bg-purple-700 text-white text-2xl px-3 py-1 rounded-xl mt-16 "
+        onClick={() => dispatch(clearTodos())}
       >
         Clear
       </button>
--- viteReact-todoProject-redux-01/src/components/TodoList.jsx
+++ viteReact-todoProject-redux-01/src/components/TodoList.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { removeTodo, toggleInputform } from "../feature/todo/todoSlice";
 
-function TodoList() {
+
+
+const TodoList = (todo) => {
+  // const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch()
+ 
   return (
-    <div>
-      
-    </div>
-  )
+        <div 
+         className="flex justify-between items-center"
+        >
+           <div className="text-white">
+            <h1>{ todo.text}</h1>
+           </div>
+           <div >
+           <button
+                  onClick={() => dispatch(toggleInputform({
+                        id: todo.id,
+                        text: todo.text,
+                      })
+                    )
+                  }
+                  className={`px-4 py-1 mr-2 rounded ${
+                    toggleInputform ? "bg-blue-700" : "bg-green-700"
+                  }`}
+                >
+                  Edit
+                </button>
+
+           <button onClick={() => dispatch(removeTodo(todo.id))}
+            className="text-white bg-red-500 border-0 px-4 py-1 focus:outline-none hover:bg-red-600 rounded text-md"
+           >X</button>
+           </div>
+
+        </div>
+  );
 }
 
-export default TodoList
+export default TodoList;
